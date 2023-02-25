1b33555b11bad49988c87c020ecaae85004d7380
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-25 16:29:31 +0000

    Terminado Ul y li

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,13 +5,16 @@ import React from "react";
 //create your first component
 const Home = () => {
 	return (
-		<div className="container col-1 my-5">
-			<h1>TODO</h1>
-			<ul className="table">
-				<li>Hola</li>
-				<li>Hola</li>
-				<li>Hola</li>
+		<div className="container">
+			<h1 className="text-center">MY TODOS</h1>
+			<ul className="p-0 mb-0">
+				<li><input type="text" placeholder="What do you need to do?"></input></li>
+				<li>Ordenar el cuarto</li>
+				<li>Enviar correos</li>
+				<li>Ir al Gym</li>
+				<li>Pagar el alquiler</li>
 			</ul>
+			<div>23 tasks</div>
 		</div>
 	);
 };
