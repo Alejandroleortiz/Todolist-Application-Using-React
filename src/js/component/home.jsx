aa18c324caa7f95212aa627c7f44f4b46fa39fbd
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-25 21:29:01 +0000

    Todos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,29 @@
-import React from "react";
-import {TiDelete} from 'react-icons/ti';
+import React, { useState } from "react";
+import { TiDelete } from 'react-icons/ti';
 
 //create your first component
 const Home = () => {
+
+	const [imputValue, setImputValue] = useState("")
+	const [todos, setTodos] = useState([])
+	
 	return (
 		<>
-		<div className="container">
-			<h1 className="text-center">MY TODOS</h1>
-			<ul className="p-0 mb-0">
-				<li><input type="text" placeholder="What do you need to do?"></input></li>
-				<li>Ordenar el cuarto <TiDelete className="iconColor"></TiDelete></li>
-				<li>Enviar correos <TiDelete className="iconColor"></TiDelete></li>
-				<li>Ir al Gym <TiDelete className="iconColor"></TiDelete></li>
-				<li>Pagar el alquiler <TiDelete className="iconColor"></TiDelete></li>
-			</ul>
-			<div>23 tasks</div>
-		</div>
+			<div className="container">
+				<h1 className="text-center">MY TODOS {imputValue}</h1>
+				<ul className="p-0 mb-0">
+					<li><input 
+					onChange={(e)=>setImputValue(e.target.value)}
+					value = {imputValue}
+					type="text" 
+					placeholder="What do you need to do?"></input></li>
+					<li>Ordenar el cuarto <TiDelete className="iconColor"></TiDelete></li>
+					<li>Enviar correos <TiDelete className="iconColor"></TiDelete></li>
+					<li>Ir al Gym <TiDelete className="iconColor"></TiDelete></li>
+					<li>Pagar el alquiler <TiDelete className="iconColor"></TiDelete></li>
+				</ul>
+				<div>4 item left</div>
+			</div>
 		</>
 	);
 };
