85ef21e1e01aaff06a190d18f13d4f6a859aaaa3
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-25 17:11:40 +0000

    Terminado Ul y li

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
-
+import {TiDelete} from 'react-icons/ti';
 
 //create your first component
 const Home = () => {
 	return (
+		<>
 		<div className="container">
 			<h1 className="text-center">MY TODOS</h1>
 			<ul className="p-0 mb-0">
 				<li><input type="text" placeholder="What do you need to do?"></input></li>
-				<li>Ordenar el cuarto</li>
-				<li>Enviar correos</li>
-				<li>Ir al Gym</li>
-				<li>Pagar el alquiler</li>
+				<li>Ordenar el cuarto <TiDelete className="iconColor"></TiDelete></li>
+				<li>Enviar correos <TiDelete className="iconColor"></TiDelete></li>
+				<li>Ir al Gym <TiDelete className="iconColor"></TiDelete></li>
+				<li>Pagar el alquiler <TiDelete className="iconColor"></TiDelete></li>
 			</ul>
 			<div>23 tasks</div>
 		</div>
+		</>
 	);
 };
 
